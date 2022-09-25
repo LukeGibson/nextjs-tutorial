899fa80ce7b6f8063ca173f1aab30eaec66d3e77
Luke Gibson <luke.gibson98@hotmail.com>
2022-09-25 16:30:53 +0100

    completed net ninja tutorials

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const About = () => {
+ return (
+  <>
+    <Head>
+      <title>Ninja List | About</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
+    <div>
+      <h1>
+        About
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit ipsa unde nisi totam ullam provident veritatis eligendi molestiae, rerum ad qui nam itaque. Blanditiis, inventore fugit rem qui veniam quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit ipsa unde nisi totam ullam provident veritatis eligendi molestiae, rerum ad qui nam itaque. Blanditiis, inventore fugit rem qui veniam quos.
+      </p>
+    </div>
+  </>
+ );
+}
+
+export default About;
